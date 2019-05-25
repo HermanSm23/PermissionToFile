c5ecfade74b7365e2e97de4ff11ceabdc4be67cc
hermansmoliar23 <hermansmoliar23@gmail.com>
2019-05-25 22:41:18 +0300

    Adding new UNIT tests

---
+++ PermissionToFileUnitTest/HelperAdminTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PermissionToFileLibraryClasses;
+
+namespace PermissionToFileUnitTest
+{
+    [TestClass]
+    public class HelperAdminTest
+    {
+        [TestMethod]
+        public void BanUserTest()
+        {
+            HelperAdmin helperBanTest = new HelperAdmin("Moderator", "222", "helper");
+            User userBanTest = new User("userUnitTest", "333", "user", false);
+
+            helperBanTest.BanUser(userBanTest);
+
+            Assert.AreEqual(true, userBanTest.Banned);
+        }
+
+        [TestMethod]
+        public void UnBanUserTest()
+        {
+            HelperAdmin helperUnBanTest = new HelperAdmin("Moderator", "222", "helper");
+            User userUnBanTest = new User("userUnBanUnitTest", "333", "user", true);
+
+            helperUnBanTest.UnBanUser(userUnBanTest);
+
+            Assert.AreEqual(false, userUnBanTest.Banned);
+        }
+    }
+}
---
+++ PermissionToFileUnitTest/ModeratorTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PermissionToFileLibraryClasses;
+
+namespace PermissionToFileUnitTest
+{
+    [TestClass]
+    public class ModeratorTest
+    {
+        [TestMethod]
+        public void BanUserTest()
+        {
+            Moderator moderBanTest = new Moderator("Moderator", "222", "moder");
+            User userBanTest = new User("userUnitTest", "333", "user", false);
+
+            moderBanTest.BanUser(userBanTest);
+
+            Assert.AreEqual(true, userBanTest.Banned);
+        }
+
+        [TestMethod]
+        public void UnBanUserTest()
+        {
+            Moderator moderUnBanTest = new Moderator("Moderator", "222", "moder");
+            User userUnBanTest = new User("userUnBanUnitTest", "333", "user", true);
+
+            moderUnBanTest.UnBanUser(userUnBanTest);
+
+            Assert.AreEqual(false, userUnBanTest.Banned);
+        }
+    }
+}
