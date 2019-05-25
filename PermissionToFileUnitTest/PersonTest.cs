ba79fcb52afeb486097de17bab88d522135e8e4d
hermansmoliar23 <hermansmoliar23@gmail.com>
2019-05-25 21:55:12 +0300

    Adding Logging UNIT test for all user

---
+++ PermissionToFileUnitTest/PersonTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PermissionToFileLibraryClasses;
+
+namespace PermissionToFileUnitTest
+{
+    [TestClass]
+    public class PersonTest
+    {
+        [TestMethod]
+        public void IsLoggingTest()
+        {
+            string usernameTest = "user";
+            string passwordTest = "pass";
+
+            User userIsLoggingTest = new User("userIsLoggingUnitTest", "333", "user", false);
+
+            Assert.AreEqual(false, userIsLoggingTest.IsLoggedIn(usernameTest, passwordTest));
+        }
+    }
+}
